4e17cb42046789db133acafe47f19c5cfdd46341
AaronQLF <haroun.guessous@mail.mcgill.ca>
2023-02-18 15:26:46 -0500

    Hello

--- project/File.py
+++ project/File.py
@@ -30,7 +30,7 @@ def continuous_sound_touch_sensor():
                 value = US_SENSOR.get_value()
                 if value is not None :
                     if (value > 40):
-                        Motor.run_forever(speed_sp=1000)
+                        Motor.on_for_seconds(speed=SpeedRPM(100), seconds=3)
                     sound.Sound(duration=0.3,pitch=pitch[int(value//10)],volume=100).play()
                 sleep (DELAY_SEC)
     except BaseException:
