24ca3499f105ceadd6eac6ecefb31c26b0c1e141
AaronQLF <haroun.guessous@mail.mcgill.ca>
2023-02-18 15:17:13 -0500

    commitintg

--- project/File.py
+++ project/File.py
@@ -11,13 +11,11 @@ from time import sleep
 
 DELAY_SEC = 0.01  # seconds of delay between measurements
 US_SENSOR_DATA_FILE = "../data_analysis/us_sensor.csv"
-SOUND = [sound.Sound(duration=0.3, pitch="A4", volume=60),sound.Sound(duration=0.3, pitch="C1", volume=100),sound.Sound(duration=0.3, pitch="D1", volume=100),sound.Sound(duration=0.3, pitch="F2", volume=100)]
-
-
+pitch =["Bb7","A#6","G#8","D#5"]
 print("Program start.\nWaiting for sensors to turn on...")
-
 TOUCH_SENSOR = TouchSensor(1)
 US_SENSOR = EV3UltrasonicSensor(2)
+Motor = Motor()
 wait_ready_sensors(True) # Input True to see what the robot is trying to initialize! False to be silent.
 print("Done waiting.")
 
@@ -28,10 +26,12 @@ def continuous_sound_touch_sensor():
                 pass
             print ("Touch Sensor pressed")
             sleep (1)
-            while not TOUCH_SENSOR.is_pressed():
+            while (1):
                 value = US_SENSOR.get_value()
                 if value is not None :
-                    Sound.play(SOUND[value//4])
+                    if (value > 40):
+                        Motor.run_forever(speed_sp=1000)
+                    sound.Sound(duration=0.3,pitch=pitch[int(value//10)],volume=100).play()
                 sleep (DELAY_SEC)
     except BaseException:
         print ("Done with the program")
