fb31f2894f297a9a4ba23e2bf3907bfe0b17d6a3
AaronQLF <88989480+AaronQLF@users.noreply.github.com>
2023-02-19 15:21:24 -0500

    Commit fin

--- project/File.py
+++ project/File.py
@@ -30,6 +30,7 @@ def continuous_sound_touch_sensor():
                 if value is not None :
                     if (value > 40):
                         Motor.set_power(100)
+                    print("Distance: ", value)
                     sound.Sound(duration=0.3,pitch=pitch[int(value//10)],volume=100).play()
                 sleep (DELAY_SEC)
     except BaseException:
