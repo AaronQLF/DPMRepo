0a1db2b0e041fc573b2e3778ad09ff4e17604e38
AaronQLF <88989480+AaronQLF@users.noreply.github.com>
2023-02-19 20:22:20 -0500

    Final commit I really fucking hope

--- project/File.py
+++ project/File.py
@@ -10,7 +10,7 @@ from utils.brick import TouchSensor, EV3UltrasonicSensor, wait_ready_sensors, re
 from time import sleep
 
 DELAY_SEC = 0.5  # seconds of delay between measurements
-pitch =["Bb7","A#6","G#8","D#5"]
+pitch =["G1","A1","G8","D5"]
 print("Program start.\nWaiting for sensors to turn on...")
 TOUCH_SENSOR = TouchSensor(1)
 EmergencyTouchSensor = TouchSensor(4)
@@ -29,18 +29,18 @@ def continuous_sound_touch_sensor():
             while (1):
                 value = US_SENSOR.get_value()
                 #Checks whether the touch sensor is pressed
-                if (TOUCH_SENSOR.is_pressed() == True):
-                    if value is not None :
-                        #if the object is not there anymore, we run the motor
-                        if (value >250):
-                            Motor.set_power(30)
-                        else :
-                            print("Distance: ", value)
-                            sound.Sound(duration=0.3,pitch=pitch[int(value//10)],volume=100).play()
-                if (EmergencyTouchSensor.is_pressed() == True):
-                    print("Emergency Stop")
+                while not EmergencyTouchSensor.is_pressed():
                     Motor.set_power(0)
-                    break
+                    print("Emergency Stop")
+                    exit()
+                while not TOUCH_SENSOR.is_pressed():
+                    value = US_SENSOR.get_value()
+                    if value is not none: # if None is givenm, then data collection failed that time
+                        print(value)
+                        if (value < 40):
+                            sound.Sound(pitch[value//10]).play()
+                        else :
+                            Motor.set_power(30)           
                 sleep (DELAY_SEC)
     except BaseException:
         print ("Done with the program")
